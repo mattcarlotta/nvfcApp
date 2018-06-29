082265c51a9f5c598166266014224598f4089c74
Matt <carlotta.matt@gmail.com>
2018-06-29 14:30:42 -0700

    Fix gpu stat charts memory leak

--- src/donutChartController.py
+++ src/donutChartController.py
@@ -26,14 +26,16 @@ class DonutChart():
         self.canvas = FigureCanvas(self.fig) # attach fig to a canvas
         self.canvas.set_size_request(self.cnvsHeight, self.cnvsWidth) # sets canvas size
         self.box.add(self.canvas) # adds canvas to specified gtkBox
-        self.anim = animation.FuncAnimation(self.fig, self.update, interval=1000, blit=True) # runs animation to update stats
+        self.anim = animation.FuncAnimation(self.fig, self.update, interval=1000) # runs animation to update stats
 
 
     def update(self, i):
         self.stat = self.getStat()
+        self.ax.clear()
+        self.ax.axis('equal') # sets circle to be even within frame
         self.ax.set_title("{0}{1}".format(self.stat, self.statType), fontsize=self.statFontSize, y=0.425) # main chart title
         self.fig.suptitle(self.supTitle, fontsize=self.supFontSize, x=self.supXPos, y=self.supYPos) # super chart title
         # creates a donut chart with opts: [stat, total-stat], circle radius, colors [blue, grey], grows clockwise at 180 degrees
         self.donut, _ = self.ax.pie([self.stat, self.maxStat-self.stat], radius=self.radius, colors=[self.a(0.6), self.b(0.6)], counterclock=False, startangle=180)
         plt.setp(self.donut, width=0.2, edgecolor='white') # sets the circle width and adds a white border color
-        # self.fig.canvas.draw() # draws new updates
+        self.fig.canvas.draw_idle() # draws new updates
